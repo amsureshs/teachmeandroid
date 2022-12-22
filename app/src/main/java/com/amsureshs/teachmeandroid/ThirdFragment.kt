5cd86523aaec4a6892b447068031f77d0df91164
Suresh <sureshs@irononetech.com>
2022-12-22 20:22:05 +0530

    initial commit

---
+++ app/src/main/java/com/amsureshs/teachmeandroid/ThirdFragment.kt
@@ -0,0 +1,33 @@
+package com.amsureshs.teachmeandroid
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ThirdFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ThirdFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_third).setOnClickListener {
+            findNavController().navigate(R.id.action_ThirdFragment_to_SecondFragment)
+        }
+    }
+}
